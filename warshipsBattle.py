b087878c65b1a8177610b2c97af14700a609a460
Anselme Guillaume <guillaume.anselme@fms-ea.com>
2023-10-25 13:34:36 +0200

    different display between target hit and missed

--- warshipsBattle.py
+++ warshipsBattle.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# Définition de la taille de la grille et des lettres de colonne
 GRID_SIZE = 10
 LETTERS = "ABCDEFGHIJ"
-previous_shots = []
 
-# Position initiale des navires
+# Initialisation du dictionnaire pour stocker les résultats des tirs précédents
+previous_shots = {}
+
+# Position initiale des navires avec des coordonnées et des états
 aircraft_carrier = {(2, 2): True, (3, 2): True, (4, 2): True, (5, 2): True, (6, 2): True}  # porte_avion en B2
 cruiser = {(1, 4): True, (1, 5): True, (1, 6): True, (1, 7): True}  # croiseur en A4
 destroyer = {(3, 5): True, (3, 6): True, (3, 7): True}  # contre_torpilleur en C5
@@ -14,20 +17,23 @@ torpedo_boat = {(5, 9): True, (6, 9): True}  # torpilleur en E9
 # Liste des navires
 ships_list = [aircraft_carrier, cruiser, destroyer, submarine, torpedo_boat]
 
+# Fonction principale du jeu
 def engage():
     print("Bienvene sur la bataille navale !   Détruisez tous les navires ennemis pour gagner")
     while not all_ships_destroyed(ships_list):
         user_input = input("Entrez les coordonnées de votre tir (exemple: 'A1' ou 'J10'): ")
         coordinate = get_user_shot(user_input)
         result = check_hit(coordinate, ships_list)
-        previous_shots.append(coordinate)       # ajouter les coordonnées au tableau
-        display_battlefield(previous_shots)     # afficher la grille avec les tirs précédents
+        update_previous_shots(coordinate, result)
+        display_battlefield(previous_shots)         # afficher la grille avec les tirs précédents
 
     print("Tous les navires ennemis ont été coulés. Vous avez gagné!")
 
+# Vérifie si les coordonnées sont valides
 def is_valid_coordinate(coordinate):
     return coordinate[0] in range(1, GRID_SIZE + 1) and coordinate[1] in range(1, GRID_SIZE + 1)
 
+# Vérifie si un tir touche un navire
 def check_hit(coordinate, ships):
     for ship in ships:
         if coordinate in ship:
@@ -39,6 +45,7 @@ def check_hit(coordinate, ships):
     print("Manqué!")
     return False
 
+# Obtenir les coordonnées du tir de l'utilisateur
 def get_user_shot(user_input):
     user_input = user_input.strip().upper()
     if len(user_input) >= 2:
@@ -51,6 +58,7 @@ def get_user_shot(user_input):
     print("Coordonnées invalides. Veuillez entrer des coordonnées valides.")
     return get_user_shot(input("Entrez les coordonnées de votre tir (exemple: 'A1' ou 'J10'): "))
 
+# Vérifie si tous les navires ont été détruits
 def all_ships_destroyed(ships):
     for ship in ships:
         for position in ship.values():
@@ -58,6 +66,14 @@ def all_ships_destroyed(ships):
                 return False
     return True
 
+# Met à jour les résultats des tirs précédents
+def update_previous_shots(coordinate, result):
+    col_letter = LETTERS[coordinate[0] - 1]         # Convertir l'indice en lettre
+    row_number = coordinate[1]
+    cell_state = "Touché" if result else "Manqué"   # Mettre à jour l'état en fonction du résultat
+    previous_shots[(col_letter, row_number)] = cell_state
+
+# Affiche la grille avec les tirs précédents
 def display_battlefield(previous_shots):
     num_rows = GRID_SIZE
     num_cols = GRID_SIZE
@@ -72,10 +88,11 @@ def display_battlefield(previous_shots):
         for col in range(num_cols):
             cell_value = ' '
             if previous_shots is not None:
-                for shot in previous_shots:
-                    if (shot[0]-1) == col and (shot[1]-1) == row:
-                        cell_value = 'O'  # Marquer la zone ciblée
-                        break
+                state = previous_shots.get((LETTERS[col], row + 1), "")
+                if state == "Touché":
+                    cell_value = 'X'
+                elif state == "Manqué":
+                    cell_value = 'O'
             row_display.append(f"[{cell_value}]")
 
         print(" ".join(row_display))
